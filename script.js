ef308415d1e4d6108d60d8c350095733e6cd6e85
Nomenyi <tnda108@gmail.com>
2022-09-22 14:41:15 +0300

    2

--- script.js
+++ script.js
@@ -1,20 +1,68 @@
 // import QrScanner from "./node_modules/qr-scanner/qr-scanner.min.js";
 
+// const camQrResult = document.getElementById('cam-qr-result');
+const flashToggle = document.getElementById('flash-toggle');
+const flashState = document.getElementById('flash-state');
 const video = document.getElementById('qr-video');
 
-const qrScanner = new QrScanner(
-    video,
-    result => alert('decoded qr code:', result),
-    { 
-        /* your options or returnDetailedScanResult: true if you're not specifying any other options */ 
-        highlightScanRegion: true,
+function setResult(result) {
+    alert(result)
+}
+
+const scanner = new QrScanner(video, result => setResult(result), {
+    onDecodeError: error => {
+        // camQrResult.textContent = error;
+        // camQrResult.style.color = 'inherit';
     },
-);
+    highlightScanRegion: true,
+    highlightCodeOutline: true,
+});
 
-qrScanner.start()
+const updateFlashAvailability = () => {
+    scanner.hasFlash().then(hasFlash => {
+        // camHasFlash.textContent = hasFlash;
+        flashToggle.style.display = hasFlash ? 'inline-block' : 'none';
+    });
+};
 
-// document.getElementById('start-button').addEventListener('click', () => {
-//     alert('scan will start')
-//     qrScanner.start()
+// scanner.start().then(() => {
+//     updateFlashAvailability();
+// })
+
+document.getElementById('start-button').addEventListener('click', () => {
+    QrScanner.hasCamera().then(
+        scanner.start().then(() => {
+            updateFlashAvailability();
+        })
+    );
+});
+
+// $('#start-button').on('click',  () => {
+//     QrScanner.hasCamera().then(
+//         scanner.start().then(() => {
+//             updateFlashAvailability();
+//         })
+//     );
+// })
+
+window.scanner = scanner;
+
+// document.getElementById('scan-region-highlight-style-select').addEventListener('change', (e) => {
+//     videoContainer.className = e.target.value;
+//     scanner._updateOverlay(); // reposition the highlight because style 2 sets position: relative
 // });
 
+// document.getElementById('show-scan-region').addEventListener('change', (e) => {
+//     const input = e.target;
+//     const label = input.parentNode;
+//     label.parentNode.insertBefore(scanner.$canvas, label.nextSibling);
+//     scanner.$canvas.style.display = input.checked ? 'block' : 'none';
+// });
+
+flashToggle.addEventListener('click', () => {
+    scanner.toggleFlash().then(() => flashState.textContent = scanner.isFlashOn() ? 'on' : 'off');
+});
+
+document.getElementById('stop-button').addEventListener('click', () => {
+    scanner.stop();
+});
